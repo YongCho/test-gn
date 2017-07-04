1c4dbe75fe6bc7fc2d82bececf764c26b5817ef5
Yong Cho <yongjec@gmail.com>
2017-07-04 15:49:27 -0400

    Copy binaries to bin directory

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 set -eu
 
 THIS_DIR="$(dirname "${0}")"
+BUILD_DIR="out"
+BIN_DIR="bin"
 
 gn_gen_args=""
 gn_gen_args="$gn_gen_args oracle_version=\"11\""
@@ -11,5 +13,8 @@ gn_gen_args="$gn_gen_args third_party_source=\"${THIRD_PARTY_SOURCE}\""
 echo "gn_gen_args:"$gn_gen_args
 
 cd "${THIS_DIR}"
-gn gen out --args="$gn_gen_args"
-ninja -C out
+gn gen "${BUILD_DIR}" --args="$gn_gen_args"
+ninja -C "${BUILD_DIR}"
+mkdir -p "${BIN_DIR}"
+cp -f "${BUILD_DIR}"/main "${BIN_DIR}"
+cp -f "${BUILD_DIR}"/*.so "${BIN_DIR}"
